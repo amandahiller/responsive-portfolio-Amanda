cd190eeb73ef4d75a9a341e5fc2e7d37295f8a24
Amanda Speroni-Hiller <amandasperoni@icloud.com>
2023-01-02 09:11:32 -0800

    Fixed swiper issue
    Signed-off-by: Amanda Speroni-Hiller <amandasperoni@icloud.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,7 +85,19 @@ modalCloses.forEach((modalClose) => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiper = new Swiper(".swiper-container", {
+    cssMode: true,
+    loop: true,
+    direction: 'horizontal',
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
 /*==================== TESTIMONIAL ====================*/
 
